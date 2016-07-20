102bdd87e287083a652fd2bb5795c2c2d24ad8b0
raihanabanu <raihanabanu.s@gmail.com>
2016-07-20 12:19:20 +0530

    Create sumn.java

---
+++ sumn.java
@@ -0,0 +1,16 @@
+# Sumn
+import java.util.Scanner;
+public class sumn {
+public static void main(String[] args) {
+		Scanner s=new  Scanner(System.in);
+		int k=s.nextInt();
+		int n=s.nextInt();
+		for(int i=0;i<=n;i++)
+		{
+			k=k+i;
+			System.out.println(k);
+		}
+
+	}
+
+}
